b93eec3b1f8d940268ef41b8370939bef2c3200e
whitespacecowboy <Imberserker@proton.me>
2026-07-19 23:19:24 +0530

    refactor(tab): remove src/ folder

---
+++ server/features/tabs/tabs.model.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+
+async function connectDbTab(): Promise<any> {
+	try {
+		await mongoose.connect('mongodb://localhost:27017/tab-manger')
+	} catch (err) {
+		console.error(err)
+	}
+}
+
+connectDbTab()
+
+const tabsSchema = new mongoose.Schema({
+	id: { type: Number, required: true },
+	index: { type: Number, required: true },
+	windowId: { type: Number, required: true },
+	lastAccessed: { type: Number, required: true },
+	groupId: { type: Number, required: true },
+	url: { type: String, required: true },
+	title: { type: String, required: true },
+	openerId: { type: Number, required: true }
+}) 
+
+export const Tabs =  mongoose.model('tabs-info', tabsSchema)
