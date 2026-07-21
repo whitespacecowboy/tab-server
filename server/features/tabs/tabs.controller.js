1b33747953dd5df1da3d8969a69e558d34288c25
whitespacecowboy <Imberserker@proton.me>
2026-07-21 21:43:17 +0530

    fix(tabs): prevent crashes on failed DB operations in tabs service

--- server/features/tabs/tabs.controller.js
+++ server/features/tabs/tabs.controller.js
@@ -1,16 +1,16 @@
 import { createTabEntry, updateTabEntry, removeTabEntry } from "./tabs.repository.js"
 
 export const handleTabCreated = (req, res) => {
-  createTabEntry(req.body)
-  res.status(200).json({ message: 'Tab created event received' });
+	createTabEntry(req.body)
+	res.status(200).json({ message: 'Tab created event received' });
 };
 
 export const handleTabRemoved = (req, res) => {
-  removeTabEntry(req.body)
-  res.status(200).json({ message: 'Tab removed event received' });
+	removeTabEntry(req.body)
+	res.status(200).json({ message: 'Tab removed event received' });
 };
 
 export const handleTabUpdated = (req, res) => {
-  updateTabEntry(req.body)
-  res.status(200).json({ message: 'Tab updated event received' });
+	updateTabEntry(req.body)
+	res.status(200).json({ message: 'Tab updated event received' });
 };
--- server/features/tabs/tabs.repository.js
+++ server/features/tabs/tabs.repository.js
@@ -1,50 +1,82 @@
-import mongoose from "mongoose"
-import Tabs from "./tabs.model.js"
+import Tabs from "./tabs.model.js";
 
 export const createTabEntry = async (tabObj) => {
-    try {
-        const status = await new Tabs(tabObj).save()
-    } catch (error) {
-        console.error(new Error("Cound't insert tab entry"), { cause: error} )
-    }
-    console.log("Tab Entry added successfully", tabObj.id)
-}
+	let status;
+	try {
+		status = await new Tabs(tabObj).save();
+	} catch (error) {
+		console.error(new Error("Couldn't insert tab entry"), { cause: error });
+		return;
+	}
+	if (status?._id) {
+		console.log("Tab entry added successfully", tabObj.id);
+	} else {
+		console.warn("Couldn't insert tab entry", tabObj.id);
+	}
+};
 
 export const updateTabEntry = async (updatedInfo) => {
-    // TODO: This function is invoked way too many times because of how onUpdated event lister works. Fix that later
-    try {
-        const status = await Tabs.updateOne(
-            { id: updatedInfo.tabId },
-            updatedInfo.changeInfo
-        )
-    } catch (error) {
-        console.error(new Error("Couldn't update tab entry"), {cause: error})
-    }
-    console.log("Tab Entry updated successfully", changeInfo.tabId)
-}
+	// TODO: This function is invoked way too many times because of how onUpdated event listener works. Fix that later
+	let status;
+	try {
+		status = await Tabs.updateOne(
+			{ id: updatedInfo.id },
+			updatedInfo.changeInfo
+		);
+	} catch (error) {
+		console.error(new Error("Couldn't update tab entry"), { cause: error });
+		return;
+	}
+	if (status.acknowledged) {
+		if (status.matchedCount === 0) {
+			console.warn("Couldn't update, no match found", updatedInfo.id);
+		} else if (status.modifiedCount === 1) {
+			console.log("Tab entry updated successfully", updatedInfo.id);
+		}
+	}
+};
 
 export const removeTabEntry = async (removeInfo) => {
-    try {
-        const status = await Tabs.deleteOne( { id: removeInfo.tabId })
-    } catch (error) {
-        console.error(new Error("Couldn't remove tab entry"), {cause: error})
-    }
-    console.log("Tab Entry removed successfully")
-}
+	let status;
+	try {
+		status = await Tabs.deleteOne({ id: removeInfo.id });
+	} catch (error) {
+		console.error(new Error("Couldn't remove tab entry"), { cause: error });
+		return;
+	}
+	if (status.acknowledged && status.deletedCount === 1) {
+		console.log("Tab entry removed successfully", removeInfo.id);
+	} else {
+		console.warn("Tab entry wasn't removed", removeInfo.id);
+	}
+};
 
-const readTabEntry = async (id) => {
-    const output = await Tabs.find()
-    console.log(output)
-}
+const readTabEntry = async (searchCondition) => {
+	let output;
+	try {
+		output = Tabs.find(searchCondition)
+	} catch (error) {
+		console.error(new Error("Couldn't read tab entries"), { cause: error });
+		return;
+	}
+	if (output.length !== 0) {
+		console.log(output);
+	} else {
+		console.log("tab-info table was empty");
+	}
+};
 
-const deleteAllTabEntries = async () => {
-    try {
-        const status = await Tabs.deleteMany({})
-    } catch (error) {
-        console.error(new Error("Couldn't delete all table entries"), {cause: error})
-    }
-    console.log("DELETED ALL TAB ENTRIES")
-}
-
-// deleteAllTabEntries()
-// readTabEntry()
+export const deleteAllTabEntries = async () => {
+	let status;
+	try {
+		status = await Tabs.deleteMany({});
+	} catch (error) {
+		console.error(new Error("Couldn't delete all table entries"), { cause: error });
+		return;
+	}
+	if (status.acknowledged && status.deletedCount > 0) {
+		console.log("tab-info is now empty");
+	} else {
+		console.warn("tab-info was already empty");
+	}
+};
