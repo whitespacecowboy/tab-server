a83927d315df354485397b7556ae9d29649f8967
whitespacecowboy <Imberserker@proton.me>
2026-07-22 16:46:40 +0530

    refactor: conforming to standard
    - change variable name to comform to a naming standard

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import app from './config/express.js';
 import connectDB from './config/database.js'
+import { deleteAllTabEntries as clear } from './features/tabs/tabs.repository.js'
 
 const PORT_EXPRESS = 3000;
 const PORT_DB = 27017;
@@ -7,11 +8,12 @@ const PORT_DB = 27017;
 async function main() {
 	try {
 		await connectDB(PORT_DB)
+		// await clear()
 		app.listen(PORT_EXPRESS, () => {
 			console.info(`listening on http://localhost:${PORT_EXPRESS}/`);
 		})
-	} catch (err) {
-		console.error(err)
+	} catch (error) {
+		console.error(error)
 	}
 }
 
